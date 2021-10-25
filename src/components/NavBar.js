fd3b3ed8ebedd5c3056603c4dcab5808902dd394
Peter Fontana <dude0508@gmail.com>
2021-10-25 12:34:40 -0400

    added some styling as a baseline

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,46 @@
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+    display: "inline-flex",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "#a79d51",
+    textDecoration: "none",
+    webkitTextStroke: "0.1px black",
+    color: "white",
+    borderRadius: "0 0 10px 10px",
+}
+
 function NavBar() {
     return (
-        <div>
+        <div style={{ display: "flex", justifyContent: "space-around", }}>
             <NavLink
                 to="/"
                 exact
+                style={linkStyles}
+                activeStyle={{
+                    background: "#625c2e",
+                }}
             >
                 Home
             </NavLink>
             <NavLink
                 to="/bookshelf"
                 exact
+                style={linkStyles}
+                activeStyle={{
+                    background: "#625c2e",
+                }}
             >
                 Bookshelf
             </NavLink>
             <NavLink
                 to="/addbook"
                 exact
+                style={linkStyles}
+                activeStyle={{
+                    background: "#625c2e",
+                }}
             >
                 Add a Book
             </NavLink>
