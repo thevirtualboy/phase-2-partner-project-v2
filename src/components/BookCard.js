2c29996bc67eaa3f095c70ade445e02c35a42407
Peter Fontana <dude0508@gmail.com>
2021-10-25 16:07:14 -0400

    styling added for header, homepage

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,8 +1,23 @@
 import React from "react";
 
+const cardStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+    border:"solid 1px #484839",
+    borderRadius: "30px", 
+    maxWidth:"400px", 
+    margin:"10px", 
+    background:"white",
+    padding: "15px",
+    boxShadow: "8px 8px 10px #A29e73",
+    flexBasis: "20%"
+}
+
 function BookCard({book:{title, author, genre, img, publishYear, description}}) {
     return (
-        <div style={{border:"solid", width:"300px", margin:"auto", background:"white"}}>
+        <div style={cardStyle}>
             <img  src={img} alt={title} />
             <h3>{title}</h3>
             <p>Author: {author}</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import BookCard from "./BookCard"
 
+const homeStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "20px",
+    justifyContent: "space-around",
+}
+
 function Home({allBooks}) {
     const displayBooks = allBooks.map(book => <BookCard key={book.id} book={book}/>)
 
     return (
-        <div>
+        <div style={homeStyle}>
             {displayBooks}
         </div>
     )
